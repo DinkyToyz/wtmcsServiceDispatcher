acd6366e5f600235134c1bd9e58341a36c90a66e
Jonas <jonas@truls.org>
2017-07-08 23:19:19 +0200

    Commented.

--- wtmcsServiceDispatcher/UI/InformationalText.cs
+++ wtmcsServiceDispatcher/UI/InformationalText.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using ColossalFramework.UI;
+﻿using ColossalFramework.UI;
 using ICities;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace WhatThe.Mods.CitiesSkylines.ServiceDispatcher
 {
@@ -20,49 +14,14 @@ namespace WhatThe.Mods.CitiesSkylines.ServiceDispatcher
         private UITextField textField;
 
         /// <summary>
-        /// The text field parent.
-        /// </summary>
-        private UIComponent textFieldParent;
-
-        /// <summary>
         /// The text field label.
         /// </summary>
         private UILabel textFieldLabel;
 
-        public bool IsVisible => this.textFieldParent.isVisible && this.textFieldLabel.isVisible;
-
-        public bool IsEnabled => this.textFieldParent.isEnabled && this.textFieldLabel.isEnabled;
-
-        public string Text
-        {
-            get => this.textFieldLabel.text;
-            set
-            {
-                this.textFieldLabel.text = value;
-            }
-        }
-
-        public void Hide()
-        {
-            this.textFieldParent.Hide();
-        }
-
-        public void Show()
-        {
-            this.textFieldParent.Show();
-            this.textField.Hide();
-        }
-
-        public void Enable()
-        {
-            this.textFieldParent.Enable();
-            this.textField.Disable();
-        }
-
-        public void Disable()
-        {
-            this.textFieldParent.Disable();
-        }
+        /// <summary>
+        /// The text field parent.
+        /// </summary>
+        private UIComponent textFieldParent;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="InformationalText"/> class.
@@ -84,6 +43,71 @@ namespace WhatThe.Mods.CitiesSkylines.ServiceDispatcher
 
             textFieldLabel = (UILabel)textFieldParent.AddUIComponent(typeof(UILabel));
             textFieldLabel.text = informationalText;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this component is enabled.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this component is enabled; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsEnabled => this.textFieldParent.isEnabled && this.textFieldLabel.isEnabled;
+
+        /// <summary>
+        /// Gets a value indicating whether this component is visible.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this component is visible; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsVisible => this.textFieldParent.isVisible && this.textFieldLabel.isVisible;
+
+        /// <summary>
+        /// Gets or sets the text.
+        /// </summary>
+        /// <value>
+        /// The text.
+        /// </value>
+        public string Text
+        {
+            get => this.textFieldLabel.text;
+            set
+            {
+                this.textFieldLabel.text = value;
+            }
+        }
+
+        /// <summary>
+        /// Disables this component.
+        /// </summary>
+        public void Disable()
+        {
+            this.textFieldParent.Disable();
+        }
+
+        /// <summary>
+        /// Enables this component.
+        /// </summary>
+        public void Enable()
+        {
+            this.textFieldParent.Enable();
+            this.textField.Disable();
+        }
+
+        /// <summary>
+        /// Hides this component.
+        /// </summary>
+        public void Hide()
+        {
+            this.textFieldParent.Hide();
+        }
+
+        /// <summary>
+        /// Shows this component.
+        /// </summary>
+        public void Show()
+        {
+            this.textFieldParent.Show();
+            this.textField.Hide();
         }
     }
 }
